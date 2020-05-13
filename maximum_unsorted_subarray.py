98466cf58350ecbebef1ce1e3612ee58eef9344c
Mayank <bunnymarwaha.bm@gmail.com>
2020-05-13 00:25:54 +0000

    maximum unsorted subarray implemented

--- maximum_unsorted_subarray.py
+++ maximum_unsorted_subarray.py
@@ -1,6 +1,16 @@
 #!/usr/local/bin/python3.7
 
-import sys
+#You are given an array (zero indexed) of N non-negative integers, A0, A1 ,…, AN-1.
+#Find the minimum sub array Al, Al+1 ,…, Ar so if we sort(in ascending order) that sub array, then the whole array should get sorted.
+#If A is already sorted, output -1.
+#
+#Example :
+#    Input 1:
+#        A = [1, 3, 2, 4, 5]
+#        Return: [1, 2]
+#    Input 2:
+#        A = [1, 2, 3, 4, 5]
+#        Return: [-1]
 
 def subUnsort(A):
     llist = []
