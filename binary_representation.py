021bb7e0529cbe6aead8890b0deeaeed0a5130e6
Mayank <bunnymarwaha.bm@gmail.com>
2020-08-06 16:30:03 +0000

    Binary Converter

---
+++ binary_representation.py
@@ -0,0 +1,18 @@
+#!/usr/local/bin/python3.7
+
+#Given a number N >= 0, find its representation in binary.
+#Example:
+#    if N = 6,
+#    binary form = 110
+
+def findDigitsInBinary(A):
+    stri = ""
+    if A == 0:
+        return 0
+    while (A > 0):
+        remainder = (A%2)
+        stri = str(remainder)+stri
+        A = (A//2)
+    return stri
+
+print(findDigitsInBinary(0))
